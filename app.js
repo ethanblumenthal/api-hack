392005b206dd6cebbc61e9089089449e12e4d640
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-25 13:06:58 -0500

    stubbed API functions

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+function searchMealByCategory(category) {
+  // let query = {
+  //   location: search,
+  //   categories: 'Breweries',
+  //   limit: 10,
+  //   sort_by: 'rating'
+  // }
+  // $.getJSON(YELP_URL, query, callback);
+}
+
+function searchCocktailByIngredient(ingredient) {
+
+}
+
+function searchMealDetailsById(mealId) {
+
+}
+
+function searchCocktailDetailsById(cocktailId) {
+
+}
+
 function handleSubmit() {
   $('#form').submit((e) => {
     e.preventDefault();
-    let meal = $('#meal').val();
-    let drink = $('#drink').val();
-    $('#meal').val('');
-    $('#drink').val('');
-    callMealApi(meal);
-    callDrinkApi(drink);
-    console.log(meal);
-    console.log(drink);
+    let category = $('#category').val();
+    let ingredient = $('#ingredient').val();
+    $('#category').val('');
+    $('#ingredient').val('');
+    searchMealByCategory(category);
+    searchCocktailByIngredient(ingredient);
   });
 }
 handleSubmit();
