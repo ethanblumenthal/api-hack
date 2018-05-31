7a543038f5fe9e8cfbb679b37aced86aa5edf594
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-30 20:42:13 -0500

    renderMealResults

--- app.js
+++ app.js
@@ -10,26 +10,39 @@ function searchDrinkByCategory(drink) {
   let url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${drink}`;
   $.getJSON(url, function(data) {
     let drinkId = data.drinks[Math.floor(Math.random() * data.drinks.length)].idDrink;
-    searchCocktailDetailsById(drinkId);
+    searchDrinkDetailsById(drinkId);
   });
 }
 
 function searchMealDetailsById(mealId) {
   let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
   $.getJSON(url, function(data) {
-    console.log(data.meals);
+    let mealDetails = data.meals[0];
+    renderMealResults(mealDetails);
   });
 }
 
-function searchCocktailDetailsById(drinkId) {
+function searchDrinkDetailsById(drinkId) {
   let url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`;
   $.getJSON(url, function(data) {
     console.log(data.drinks);
   });
 }
 
+function renderMealResults(mealDetails) {
+  $('#search').html(
+    `<h1>${mealDetails.strMeal}</h1>
+    <img src="${mealDetails.strMealThumb}" />
+    <p>${mealDetails.strInstructions}</p>`
+  );
+}
+
+function renderDrinkResults() {
+  
+}
+
 function handleSubmit() {
-  $('#form').submit((e) => {
+  $('#search').submit((e) => {
     e.preventDefault();
     let meal = $('#meal').val();
     let drink = $('#drink').val();
